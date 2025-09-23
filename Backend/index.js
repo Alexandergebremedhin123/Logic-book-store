ceb10ec4a2efa495c6e523a7fd7b720a645003fb
Mr. Alexander <100361766+Alexandergebremedhin123@users.noreply.github.com>
2025-09-23 08:31:53 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,22 +11,11 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
-const allowedOrigins = [
-  'http://localhost:3000',             
-  'https://logic-book-store.vercel.app'   
-];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true); 
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET','POST','PUT','DELETE'],
-  allowedHeaders: ['Content-Type','Authorization']
+  origin:  'https://logic-book-store.vercel.app', 
+  methods: "GET,POST,PUT,DELETE",   
+  credentials: true              
 }));
 
 const MyBooks = require('./books/books');
